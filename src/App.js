a23dd818d15bccfa33ba48ad0ae9f629cc27f8a4
Joshua Paul <veecthorpaul@gmail.com>
2021-02-03 18:54:26 +0100

    react app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,27 @@ import About from './components/About'
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([
+
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true
+    },
+    {
+      text: 'Code',
+      day: 'Today',
+      reminder: true,
+      id: 3
+    },
+  ])
 
   useEffect(() => {
     const getTasks = async () => {
